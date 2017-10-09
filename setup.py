a17256ba9c364ff5e9af4dce61ee7c4f82a6016b
Andrew Jackson <Andrew.Jackson@bl.uk>
2017-10-09 23:08:11 +0100

    Adding in initial module removed from warcprox.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='warcprox-plugins',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Greg Ward',
+      author_email='gward@python.net',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['warcprox-plugins'],
+    install_requires=[
+        "warcprox",
+        "kafka-python"
+    ],
+    dependency_links=[
+        "git+https://github.com/internetarchive/warcprox.git#egg=warcprox"
+    ]
+     )
