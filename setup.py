e8b6ba2d76f45473c32b7f3e5abf505cda5c5f01
Andrew Jackson <Andrew.Jackson@bl.uk>
2019-03-04 12:30:12 +0000

    Fix version at 2.3

--- setup.py
+++ setup.py
@@ -3,18 +3,15 @@
 from setuptools import setup, find_packages
 
 setup(name='warcprox-plugins',
-      version='1.0',
+      version='2.3',
       description='Extending warcprox with UKWA plugins',
       author='Andrew Jackson',
       author_email='andrew.jackson@bl.uk',
       url='https://www.github.com/ukwa/docker-warcprox/',
       packages=['warcprox-plugins'],
       install_requires=[
-          "warcprox",
+          "warcprox==2.3",
           "kafka-python",
           "requests"
-      ],
-      dependency_links=[
-          "git+https://github.com/internetarchive/warcprox.git#egg=warcprox"
       ]
       )
