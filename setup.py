c7969a2ddf6b908ce6814ad691be0d3f42bf2bde
Andrew Jackson <Andrew.Jackson@bl.uk>
2020-02-13 21:48:53 +0000

    Update warcprox version.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(name='warcprox-plugins',
-      version='2.3',
+      version='2.4.17',
       description='Extending warcprox with UKWA plugins',
       author='Andrew Jackson',
       author_email='andrew.jackson@bl.uk',
       url='https://www.github.com/ukwa/docker-warcprox/',
       packages=['warcprox-plugins'],
       install_requires=[
-          "warcprox==2.3",
+          "warcprox==2.4.17",
           "kafka-python",
           "requests"
       ]
